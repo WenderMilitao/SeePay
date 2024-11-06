94a93b70b17554113727e2e16373f06e0c43e3ce
Caroline Marchi <cah.de.marchi@hotmail.com>
2024-11-06 11:56:55 -0300

    feat: exibir o saldo no card do componente despesas

--- src/app/pages/despesas/despesas.component.ts
+++ src/app/pages/despesas/despesas.component.ts
@@ -2,39 +2,63 @@ import { Component, inject } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
 import { DespesaService } from 'src/app/services/despesa.service';
+import { RendimentoService } from './../../services/rendimento.service';
 
 @Component({
   selector: 'app-despesas',
   templateUrl: './despesas.component.html',
-  styleUrls: ['./despesas.component.css']
+  styleUrls: ['./despesas.component.css'],
 })
 export class DespesasComponent {
-
   private breakpointObserver = inject(BreakpointObserver);
-  valorTotal: number = 0;
-  constructor(private despesaService: DespesaService) {
+  valorTotalRendimentos: number = 0;
+  valorTotalDespesas: number = 0;
+  saldo: number = 0;
+
+  constructor(
+    private rendimentoService: RendimentoService,
+    private despesaService: DespesaService
+  ) {
+    // Calculando o valor total das despesas
     this.despesaService.calcularValorTotal().subscribe({
       next: (total) => {
-        console.log('Total calculado:', total);
-        this.valorTotal = total;
+        console.log('Total de despesas calculado:', total);
+        this.valorTotalDespesas = total;
+        this.calcularSaldo(); // Recalcula o saldo após calcular as despesas
       },
       error: (err) => {
-        console.error('Erro ao calcular total:', err);
+        console.error('Erro ao calcular despesas:', err);
+      },
+    });
+
+    // Calculando o valor total dos rendimentos
+    this.rendimentoService.calcularValorTotal().subscribe({
+      next: (total) => {
+        console.log('Total de rendimentos calculado:', total);
+        this.valorTotalRendimentos = total;
+        this.calcularSaldo(); // Recalcula o saldo após calcular os rendimentos
+      },
+      error: (err) => {
+        console.error('Erro ao calcular rendimentos:', err);
       },
     });
   }
-  // Observável que fornece o layout dinâmico com base no tamanho da tela
+
+  // Função para calcular o saldo (rendimentos - despesas)
+  calcularSaldo(): void {
+    this.saldo = this.valorTotalRendimentos - this.valorTotalDespesas;
+  }
+
+  // Layout dinâmico para responsividade
   cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        // Layout para telas de mão (Handset)
         return {
           columns: 2,
           chart: { cols: 1, rows: 2 },
           table: { cols: 1, rows: 2 },
         };
       } else {
-        // Layout para telas maiores (desktop)
         return {
           columns: 2,
           chart: { cols: 1, rows: 2 },
@@ -44,5 +68,3 @@ export class DespesasComponent {
     })
   );
 }
-
-
