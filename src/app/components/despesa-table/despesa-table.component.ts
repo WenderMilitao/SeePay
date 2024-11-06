31220da8eb9dd638bc1c0e0a9eb7fd58ca3a81bb
Caroline Marchi <cah.de.marchi@hotmail.com>
2024-11-06 10:34:44 -0300

    fix: ajuste css e paginação da tabela despesas

--- src/app/components/despesa-table/despesa-table.component.ts
+++ src/app/components/despesa-table/despesa-table.component.ts
@@ -1,22 +1,23 @@
-import { Component, ViewChild } from '@angular/core';
-import { Despesa } from './../../models/despesa';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { DespesaService } from './../../services/despesa.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
-import { EditarDespesasComponent } from '../editar-despesas/editar-despesas.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Despesa } from './../../models/despesa';
+import { EditarDespesasComponent } from '../editar-despesas/editar-despesas.component';
 
 @Component({
   selector: 'app-despesa-table',
   templateUrl: './despesa-table.component.html',
   styleUrls: ['./despesa-table.component.css'],
 })
-export class DespesaTableComponent {
+export class DespesaTableComponent implements AfterViewInit {
   despesasList!: Despesa[];
-  dataSource: any;
+  dataSource: MatTableDataSource<Despesa> = new MatTableDataSource();
   displayedColumns: string[] = ['despesa', 'valor', 'data', 'actions'];
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -27,40 +28,44 @@ export class DespesaTableComponent {
   ) {
     this.despesaService.getAllDespesas().subscribe((res) => {
       this.despesasList = res;
-      this.dataSource = new MatTableDataSource<Despesa>(this.despesasList);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.dataSource.data = this.despesasList;
     });
-    this.loadDespesas()
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   loadDespesas() {
     this.despesaService.getAllDespesas().subscribe((data) => {
-      this.dataSource = data;
+      this.dataSource.data = data;
     });
   }
-  abrirModal(despesa:Despesa) {
+
+  abrirModal(despesa: Despesa) {
     const dialogRef = this.dialog.open(EditarDespesasComponent, {
-      data: despesa, // Passando a despesa para o modal
+      data: despesa,
       width: '45%',
       height: '400px',
     });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.loadDespesas(); // Recarrega a lista após edição
+        this.loadDespesas();
       }
     });
   }
+
   deleteDespesa(despesaId: string) {
     if (confirm('Você tem certeza que deseja excluir esta despesa?')) {
       this.despesaService.deleteDespesa(despesaId).subscribe(
         () => {
-          this.loadDespesas(); // Recarrega a lista de despesas
+          this.loadDespesas();
           this.snackBar.open('Despesa excluída com sucesso!', 'Fechar', {
             duration: 2000,
           });
         },
         (error) => {
-          console.error('Erro ao excluir despesa: ', error);
           this.snackBar.open('Erro ao excluir despesa.', 'Fechar', {
             duration: 2000,
           });
