2d10cfa0e89d019286efa433435103359b4f5234
Caroline Marchi <cah.de.marchi@hotmail.com>
2024-11-06 10:17:09 -0300

    fix: ajuste css e paginação da tabela rendimentos

--- src/app/components/rendimento-table/rendimento-table.component.ts
+++ src/app/components/rendimento-table/rendimento-table.component.ts
@@ -1,22 +1,23 @@
-import { Component, ViewChild } from '@angular/core';
-import { Rendimento } from './../../models/rendimento';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RendimentoService } from './../../services/rendimento.service';
-import { EditarRendimentosComponent } from './../editar-rendimentos/editar-rendimentos.component';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Rendimento } from './../../models/rendimento';
+import { EditarRendimentosComponent } from './../editar-rendimentos/editar-rendimentos.component';
 
 @Component({
   selector: 'app-rendimento-table',
   templateUrl: './rendimento-table.component.html',
   styleUrls: ['./rendimento-table.component.css'],
 })
-export class RendimentoTableComponent {
+export class RendimentoTableComponent implements AfterViewInit {
   rendimentosList!: Rendimento[];
-  dataSource: any;
+  dataSource: MatTableDataSource<Rendimento> = new MatTableDataSource();
   displayedColumns: string[] = ['rendimento', 'valor', 'data', 'actions'];
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -27,42 +28,44 @@ export class RendimentoTableComponent {
   ) {
     this.rendimentoService.getAllRendimentos().subscribe((res) => {
       this.rendimentosList = res;
-      this.dataSource = new MatTableDataSource<Rendimento>(
-        this.rendimentosList
-      );
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.dataSource.data = this.rendimentosList;
     });
-    this.loadRendimentos();
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   loadRendimentos() {
     this.rendimentoService.getAllRendimentos().subscribe((data) => {
-      this.dataSource = data;
+      this.dataSource.data = data;
     });
   }
+
   abrirModal(rendimento: Rendimento) {
     const dialogRef = this.dialog.open(EditarRendimentosComponent, {
-      data: rendimento, // Passando o rendimento para o modal
+      data: rendimento,
       width: '45%',
       height: '400px',
     });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.loadRendimentos(); // Recarrega a lista após edição
+        this.loadRendimentos();
       }
     });
   }
+
   deleteRendimento(rendimentoId: string) {
     if (confirm('Você tem certeza que deseja excluir este rendimento?')) {
       this.rendimentoService.deleteRendimento(rendimentoId).subscribe(
         () => {
-          this.loadRendimentos(); // Recarrega a lista de rendimentos
+          this.loadRendimentos();
           this.snackBar.open('Rendimento excluído com sucesso!', 'Fechar', {
             duration: 2000,
           });
         },
         (error) => {
-          console.error('Erro ao excluir rendimento: ', error);
           this.snackBar.open('Erro ao excluir rendimento.', 'Fechar', {
             duration: 2000,
           });
